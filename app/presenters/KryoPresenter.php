4ea466890cd7051d10a5e1dda4f6621102acf63e
Vit Habada <vithabada93@gmail.com>
2016-06-29 14:13:13 +0200

    kryo: today widget

--- app/presenters/KryoPresenter.php
+++ app/presenters/KryoPresenter.php
@@ -9,6 +9,7 @@ use App\Model\SmtpMailer;
 use Libs\BootstrapForm;
 use Nette\Application\UI\Form;
 use Nette\InvalidArgumentException;
+use Nette\Utils\DateTime;
 
 class KryoPresenter extends BasePresenter
 {
@@ -31,6 +32,78 @@ class KryoPresenter extends BasePresenter
         $this->notificationMailer = new NotificationMail($this->createTemplate(), $this->smtpMailer);
     }
 
+    /**
+     * Mark order as completed.
+     *
+     * @param int $id
+     */
+    function actionComplete($id)
+    {
+        $affected = $this->orderManager->completePendingOrder($id);
+
+        if ($affected > 0) {
+            $order = $this->orderManager->find($id);
+            $this->notificationMailer
+                ->addTo($order->uzivatele->email)
+                ->setTemplateFile('notification.latte')
+                ->setSubject('Objednávka byla vyřízena!')
+                ->setTemplateVar('order', $order)
+                ->send();
+        }
+
+        if (!$this->isAjax()) {
+            if ($affected == 0) {
+                $this->flashMessage("Objednávka č. $id neexistuje, nebyla označena jako nevyřízená nebo byla stornovaná.", 'danger');
+            } else {
+                $this->flashMessage('Objednávka byla vyřízena.', 'success');
+            }
+
+            $ref = $this->getParameter('ref');
+            if ($ref) {
+                $this->redirect($ref);
+            } else {
+                $this->redirect('default');
+            }
+        }
+    }
+
+    /**
+     * Mark order as cancelled.
+     *
+     * @param $id
+     */
+    function actionCancel($id)
+    {
+        $affected = $this->orderManager->cancelPendingOrder($id);
+
+        if (!$this->isAjax()) {
+            if ($affected == 0) {
+                $this->flashMessage("Objednávka č. $id neexistuje nebo nebyla označena jako nevyřízená.", 'danger');
+            } else {
+                $this->flashMessage('Objednávka byla zrušena.', 'info');
+            }
+
+            $ref = $this->getParameter('ref');
+            if ($ref) {
+                $this->redirect($ref);
+            } else {
+                $this->redirect('default');
+            }
+        }
+    }
+
+    function renderDefault()
+    {
+        $this->template->today = $this->orderManager->getPendingOrders()
+            ->where('DATE(datum_vyzvednuti) = DATE(?)', new DateTime())
+            ->order('created DESC')
+            ->limit(10);
+    }
+
+    /**
+     * @param Form $form
+     * @param $values
+     */
     public function finishOrderFormSucceeded(Form $form, $values)
     {
         try {
@@ -48,6 +121,9 @@ class KryoPresenter extends BasePresenter
         $this->redirect('this');
     }
 
+    /**
+     * @return Form
+     */
     protected function createComponentFinishOrderForm()
     {
         $form = new Form();
@@ -69,32 +145,6 @@ class KryoPresenter extends BasePresenter
     }
 
     /**
-     * @param Form $form
-     * @param $values
-     */
-    function completeOrderFormSucceeded(Form $form, $values)
-    {
-        $affected = $this->orderManager->completePendingOrder($values->obj_id);
-
-        if ($affected > 0) {
-            $order = $this->orderManager->find($values->obj_id);
-
-            $this->notificationMailer
-                ->addTo($order->uzivatele->email)
-                ->setTemplateFile('notification.latte')
-                ->setSubject('Objednávka byla vyřízena!')
-                ->setTemplateVar('order', $order)
-                ->send();
-
-            $this->flashMessage('Objednávka byla vyřízena');
-        } else {
-            $this->flashMessage('Objednávku se nepodařilo dokončit (není označena jako nevyřízená?).', 'warning');
-        }
-
-        $this->redirect('this');
-    }
-
-    /**
      * @return Form
      */
     protected function createComponentCompleteOrderForm()
@@ -107,7 +157,9 @@ class KryoPresenter extends BasePresenter
             ->setRequired(FORM_REQUIRED);
         $form->addSubmit('process', 'Vyřídit');
 
-        $form->onSuccess[] = $this->completeOrderFormSucceeded;
+        $form->onSuccess[] = function (Form $form, $values) {
+            $this->actionComplete($values->obj_id);
+        };
 
         return BootstrapForm::makeBootstrap($form);
     }
