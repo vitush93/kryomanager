5ed4782ab1900a57f41b41c453fb52195997e1df
Vit Habada <vithabada93@gmail.com>
2017-02-22 17:31:35 +0100

    router fix

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -10,14 +10,14 @@ use Nette\Application\Routers\Route;
 class RouterFactory
 {
 
-	/**
-	 * @return Nette\Application\IRouter
-	 */
-	public static function createRouter()
-	{
-		$router = new RouteList;
-		$router[] = new Route('//%host%/<presenter>/<action>[/<id>]', 'Homepage:default');
-		return $router;
-	}
+    /**
+     * @return Nette\Application\IRouter
+     */
+    public static function createRouter()
+    {
+        $router = new RouteList;
+        $router[] = new Route('<presenter>/<action>[/<id>]', 'Homepage:default');
+        return $router;
+    }
 
 }
